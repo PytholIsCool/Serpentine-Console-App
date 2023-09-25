7b5ed55ddc5076d2e52824b90eddbb76087b03cb
Pythol <95198306+PytholIsCool@users.noreply.github.com>
2023-09-25 08:21:07 -0500

    Update Program.cs
    Added date and time to error logs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Diagnostics;
 using System.Threading;
 
@@ -74,11 +74,47 @@ MUAHAHAHHAHAHAHAHA
         private static void DisplayErrorAndRestart()
         {
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("[Serpentine] [Error] ~~> Invalid input value. Press enter to try again.");
-
+            Console.WriteLine($" [{DateTime.Now:yyyy-MM-dd HH:mm:ss}] [Serpentine] ~~> " + "Invalid user input. Press any key to try again.");
+            
             Console.ReadKey();
             Console.Clear();
             Main(new string[0]);
         }
     }
 }
+
+/*
+This C# code is a simple console application called "Serpentine_Console_App." It displays a welcome message with some ASCII art, provides a menu for the user to choose from, and opens URLs in the default web browser based on the user's selection. Let's break down the code step by step:
+
+1. Importing Necessary Libraries:
+   - The code imports three namespaces: `System`, `System.Diagnostics`, and `System.Threading`, which provide various classes and functionalities for console applications.
+
+2. Main Method:
+   - The `Main` method is the entry point of the program. It takes command-line arguments as an array of strings (`args`).
+
+3. Displaying ASCII Art:
+   - The program starts by defining an array of `ConsoleColor` enums named `consoleColors`.
+   - It then defines a multiline string `titleText`, which contains a large ASCII art design and some additional text.
+
+4. Changing Console Text Color:
+   - It sets the console text color to magenta using `Console.ForegroundColor` and then displays the `titleText`.
+
+5. Reading User Input:
+   - The program reads a line of user input from the console and stores it in the `input` variable.
+
+6. Menu Selection:
+   - It uses a `switch` statement to perform different actions based on the user's input:
+     - If the input is "1," it opens a URL in the default browser and exits.
+     - If the input is "2," it does the same for a different URL.
+     - If the input is "3," it displays an amusing message and opens another URL.
+     - If the input is "4," it exits the program.
+     - If the input is anything else, it displays an error message and restarts the program.
+
+7. Opening URLs:
+   - The `OpenLinkAndExit` method takes a URL as an argument, opens it in the default web browser using `Process.Start(url)`, displays a message to close the program, waits for any key press, and then exits the program.
+
+8. Handling Errors:
+   - The `DisplayErrorAndRestart` method displays an error message in red, logs the timestamp, waits for a key press, clears the console, and then calls the `Main` method again to restart the program.
+
+Overall, this code provides a simple interactive menu for the user to open specific URLs in a web browser or exit the program. If the user enters an invalid option, it displays an error message and allows them to try again.
+*/
